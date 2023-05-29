c9aa30f28b903b2d3f79b013664a2e7797cc9d65
Michael Ilyin <milyin@gmail.com>
2023-05-30 00:30:12 +0200

    storage type

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::sync::atomic::AtomicI32;
+
+#[repr(C)]
+pub struct Storage<'a, T: 'a> {
+    size: usize,
+    data: &'a mut [T],
+    locks: &'a mut [AtomicI32],
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<'a, T> Storage<'a, T> {
+    pub fn new<const SIZE: usize>(
+        data: &'a mut [T; SIZE],
+        locks: &'a mut [AtomicI32; SIZE],
+    ) -> Self {
+        Self {
+            size: SIZE,
+            data,
+            locks,
+        }
     }
+}
+
+#[test]
+fn test() {
+    let mut data = [0; 10];
+    let mut locks = [(); 10].map(|_| AtomicI32::new(0));
+    let storage = Storage::new(&mut data, &mut locks);
+    assert_eq!(storage.size, 10);
 }
