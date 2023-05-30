e829c17580e753d9466f98fbd3f72b5048af1aa0
Michael Ilyin <milyin@gmail.com>
2023-05-30 23:28:21 +0200

    lock structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,29 @@
-use std::sync::atomic::AtomicI32;
+use std::sync::atomic::{AtomicBool, AtomicI32};
+
+#[repr(C)]
+pub struct Lock {
+    write: AtomicBool,
+    refcount: AtomicI32,
+}
+
+impl Default for Lock {
+    fn default() -> Self {
+        Self {
+            write: AtomicBool::new(false),
+            refcount: AtomicI32::new(0),
+        }
+    }
+}
 
 #[repr(C)]
 pub struct Storage<'a, T: 'a> {
     size: usize,
     data: &'a mut [T],
-    locks: &'a mut [AtomicI32],
+    locks: &'a mut [Lock],
 }
 
 impl<'a, T> Storage<'a, T> {
-    pub fn new<const SIZE: usize>(
-        data: &'a mut [T; SIZE],
-        locks: &'a mut [AtomicI32; SIZE],
-    ) -> Self {
+    pub fn new<const SIZE: usize>(data: &'a mut [T; SIZE], locks: &'a mut [Lock; SIZE]) -> Self {
         Self {
             size: SIZE,
             data,
@@ -23,7 +35,7 @@ impl<'a, T> Storage<'a, T> {
 #[test]
 fn test() {
     let mut data = [0; 10];
-    let mut locks = [(); 10].map(|_| AtomicI32::new(0));
+    let mut locks = [(); 10].map(|_| Lock::default());
     let storage = Storage::new(&mut data, &mut locks);
     assert_eq!(storage.size, 10);
 }
